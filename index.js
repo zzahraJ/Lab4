e69d55a6407c74fb1338871cf83354d3b554ee02
zzahraJ <r0883814@student.thomasmore.be>
2023-11-14 17:53:47 +0100

    feat: add get function

--- index.js
+++ index.js
@@ -1,19 +1,65 @@
-const express = require('express')
-const app = express()
-const port = 3000
+// const express = require('express')
+// const app = express()
+// const port = 3000
 
-//enable CORS
-const cors = require('cors')
-app.use(cors())
+// //enable CORS
+// const cors = require('cors')
+// app.use(cors())
+
+// app.get('/api/v1/messages', (req, res) => {
+//   res.json({
+//     "status": "success",
+//     "message": "GET messages",
+//     "data": [],
+//   })
+// })
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
+
+
+
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// enable CORS
+const cors = require("cors");
+app.use(cors());
+app.use(express.json()); 
 
 app.get('/api/v1/messages', (req, res) => {
-  res.json({
-    "status": "success",
-    "message": "GET messages",
-    "data": [],
-  })
-})
+  const user = req.query.user;
+  if (user) {
+    res.json({
+      status: "success",
+      message: `GET messages for user ${user}`,
+    });
+  } else {
+    res.json({
+      status: "success",
+      message: "GET messages",
+      data: [
+        {
+          user: "Zahra",
+          message: "Hihihi",
+        },
+        {
+          user: "Eva",
+          message: "Hahaha",
+        },
+      ],
+    });
+  }
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.get('/api/v1/messages/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+    res.json({
+      status: "success",
+      message: `GET message with ID ${id}`,
+    });
+});
+
